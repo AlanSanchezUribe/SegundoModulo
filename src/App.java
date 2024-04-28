b8fcdc075b176ccad26db9ad215fc5df17129c64
Alan Sanchez <agsanchezu@gmail.com>
2024-04-28 09:52:39 -0400

    Fin Proyecto modulo 1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnPlan = true;
+        double notaDeLaPerfil = 8.2;
+        String tipoPlan = "plus";
+
+        if (fechaDeLanzamiento >= 2022) {
+            System.out.println(" Películas más populares");
+    }else {
+        System.out.println(" Película Retro que vale la pena ver");
+    }
+
+    if (incluidoEnPlan && tipoPlan.equals("plus")) {
+        System.out.println(" Disfrute de su películas");
+    }else {
+        System.out.println(" Película no disponible en este plan");
+    }
     }
 }
